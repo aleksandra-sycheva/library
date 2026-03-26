f5f14ed5962dca37421210fbcbb6116d09bb4b08
Александра Сычева <sashasychevaisip@gmail.com>
2026-03-26 13:30:50 +0500

    Реализован вход

---
+++ FormBooks.cs
@@ -0,0 +1,35 @@
+﻿using library.Models;
+
+namespace library
+{
+    public partial class FormBooks : Form
+    {
+        public User CurrentUser { get; private set; }
+        public bool IsGuest { get; private set; }
+        public FormBooks(User user, bool guest)
+        {
+            InitializeComponent();
+            var colPhoto = new DataGridViewImageColumn();
+            colPhoto.Name = "colPhoto";
+            colPhoto.ImageLayout = DataGridViewImageCellLayout.Zoom;
+            colPhoto.Width = 200;
+            colPhoto.FillWeight = 30;
+
+            var colInfo = new DataGridViewTextBoxColumn();
+            colInfo.Name = "colInfo";
+            colInfo.FillWeight = 60;
+            colInfo.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+
+            var colDiscount = new DataGridViewTextBoxColumn();
+            colDiscount.Name = "colDiscount";
+            colDiscount.FillWeight = 10;
+            colDiscount.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            CurrentUser = user;
+            IsGuest = guest;
+
+            
+
+        }
+    }
+}
